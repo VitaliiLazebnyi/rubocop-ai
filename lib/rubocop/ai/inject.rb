7b5e80eb55163f5165e1434af9f2d8439ac83688
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2026-04-18 05:15:02 +0900

    Fix final Sorbet typed strong enforcement: extract Object#tap into explicit evaluation bounds to circumvent untyped structural inference failures

--- lib/rubocop/ai/inject.rb
+++ lib/rubocop/ai/inject.rb
@@ -17,7 +17,8 @@ module RuboCop
         path = File.join(RuboCop::AI.project_root, 'config', 'default.yml')
         # We explicitly cast to handle missing typing in RuboCop YAML internals
         hash = T.cast(RuboCop::ConfigLoader.send(:load_yaml_configuration, path), T::Hash[T.untyped, T.untyped])
-        config = RuboCop::Config.new(hash, path).tap { |c| c.make_excludes_absolute }
+        config = RuboCop::Config.new(hash, path)
+        config.make_excludes_absolute
         RuboCop::ConfigLoader.instance_variable_set(:@default_configuration, RuboCop::ConfigLoader.default_configuration.merge(config))
       end
     end
